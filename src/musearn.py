de5d2d36cfd4e120c6e8d91f225ba5f00e10993a
xzadra03 <xzadra03@vutbr.cz>
2021-11-03 18:30:10 +0100

    first try

--- src/musearn.py
+++ src/musearn.py
@@ -1,12 +1,26 @@
-from typing import Text
 import kivy
-from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+from kivymd.uix.screen import Screen
+from kivymd.uix.button import MDRaisedButton
+from kivymd.uix.gridlayout import GridLayout
 
-class Musearn(App):
+
+class Musearn(MDApp):
+    screen = Screen()
     def build(self):
-        return Label(Text = "ITU app")
+        screen = Screen()
+        logo = MDLabel(text="Musearn", halign="center", valign="center", font_style="H3")
+        button_lessons = MDRaisedButton(text="Seznam lekci")
+        button_editor = MDRaisedButton(text="Vytvoř lekci")
+        button_songs = MDRaisedButton(text="Zahrej si")
+        screen.add_widget(logo)
+        screen.add_widget(button_lessons)
+        screen.add_widget(button_editor)
+        screen.add_widget(button_songs)
+        return screen
+
 
 if __name__ == "__main__":
     Musearn().run()
-
