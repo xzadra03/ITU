9a4f03fcdbea3c1f6526d4285f4c89f5127d59d9
xzadra03 <xzadra03@vutbr.cz>
2021-11-03 17:01:37 +0100

    hoj

--- src/musearn.py
+++ src/musearn.py
@@ -1,1 +1,12 @@
+from typing import Text
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+class Musearn(App):
+    def build(self):
+        return Label(Text = "ITU app")
+
+if __name__ == "__main__":
+    Musearn().run()
 
